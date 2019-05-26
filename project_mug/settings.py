f485f424575ac0574d3d322be2e7abf493e432f7
Gokul Gopan <gookulgopan@gmail.com>
2019-05-26 14:34:35 +0000

    added ip address and static root

--- project_mug/settings.py
+++ project_mug/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wkbhq=^5c+x66_xnismtea0-t!s+z@w(cq13(%n0_4*@9c$@ch'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['104.154.171.202']
 
 
 # Application definition
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+

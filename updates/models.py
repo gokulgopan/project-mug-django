8b33d0f9313b5fc947c31333265eaa1c6cc62db9
Gokul G <GG00467061@techmahindra.com>
2019-05-30 13:25:16 +0530

    added author to model Update

--- updates/models.py
+++ updates/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -7,6 +8,7 @@ class Update(models.Model):
     title = models.CharField(max_length=100, help_text='Enter the Title')
     body = models.TextField()
     date_pub = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete='CASCADE', default="None")
     
     def create(self):
         self.save()

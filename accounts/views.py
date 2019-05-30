e653841d563f1fb917563f2c7b923f0a381f2610
Gokul G <GG00467061@techmahindra.com>
2019-05-30 09:18:53 +0530

    login successfully worked

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
@@ -12,3 +12,14 @@ def signup_view(request):
     else:       
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form':form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('updates:index')
+    else:
+        form =AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form':form})
+
+

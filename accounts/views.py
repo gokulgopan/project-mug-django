05d7d863f701d2a5edf9a558658820f83aa7c1ae
Gokul Gopan <gookulgopan@gmail.com>
2019-05-30 18:52:21 +0530

    signup all field added

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from . import forms
 
 # Create your views here.
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = forms.SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('updates:index')
     else:       
-        form = UserCreationForm()
+        form = forms.SignUpForm()
     return render(request, 'accounts/signup.html', {'form':form})
+
+
 
 def login_view(request):
     if request.method == 'POST':
